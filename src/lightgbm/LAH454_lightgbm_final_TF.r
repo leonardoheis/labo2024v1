2af748917d8b6f0cc6098320a5157e336aa5dfd4
leonardoheis <leonardo.heis@gmail.com>
2024-04-16 17:18:51 -0300

    mejora nuevo script TF

--- src/lightgbm/LAH454_lightgbm_final_TF.r
+++ src/lightgbm/LAH454_lightgbm_final_TF.r
@@ -18,11 +18,11 @@ PARAM$input$training <- c(202107) # meses donde se entrena el modelo
 PARAM$input$future <- c(202109) # meses donde se aplica el modelo
 
 
-PARAM$finalmodel$num_iterations <- 559
-PARAM$finalmodel$learning_rate <- 0.0100746999
-PARAM$finalmodel$feature_fraction <- 0.5144127527
-PARAM$finalmodel$min_data_in_leaf <- 505
-PARAM$finalmodel$num_leaves <- 44
+PARAM$finalmodel$num_iterations <- 543
+PARAM$finalmodel$learning_rate <- 0.036838570723779
+PARAM$finalmodel$feature_fraction <- 0.201559044166511
+PARAM$finalmodel$min_data_in_leaf <- 465
+PARAM$finalmodel$num_leaves <- 36
 
 
 PARAM$finalmodel$max_bin <- 31
